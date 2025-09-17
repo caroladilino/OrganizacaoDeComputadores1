b14e1b353e18e420b069f2779671505d5e819812
Carolina Adilino <cmqadilino@gmail.com>
2025-09-16 21:04:53 -0300

    Update CalculoBaskharaInput.asm

--- Lab1/CalculoBaskharaInput.asm
+++ Lab1/CalculoBaskharaInput.asm
@@ -21,7 +21,7 @@ main:
 	
 	li 	      $v0, 5       #Comando para ler inteiro.
 	syscall
-	move 	    $t1, $v0
+	move 	  $t1, $v0
 	
 	li 	      $v0, 4       #Comando.
 	la 	      $a0, mem2    #Carrega string (endereço).
@@ -29,7 +29,7 @@ main:
 	
 	li 	      $v0, 5       #Comando para ler inteiro.
 	syscall
-	move 	    3$t0, $v0
+	move 	  3$t0, $v0
 	
 	li 	      $v0, 4       #Comando
 	la 	      $a0, mem3    #Carrega string (endereço).
@@ -37,55 +37,56 @@ main:
 	
 	li 	      $v0, 5       #Comando para ler inteiro.
 	syscall
-	move 	    $t2, $v0
+	move 	  $t2, $v0
 	
 	#Registradores $t0 até $t2 para variaveis 
 	
-	mul 	    $t4, $t0, $t0       #b²
+	mul 	  $t4, $t0, $t0        #b²
 	
-	sll       $t5, $t1,  2 		    #4a
+	sll       $t5, $t1,  2 		   #4a
 	
-	mul       $t6 , $t2 , $t5 	  #4a . c
+	mul       $t6 , $t2 , $t5 	   #4a . c
 	
-	sub       $t7 , $t4 , $t6 		#b² - 4ac
+	sub       $t7 , $t4 , $t6	   #b² - 4ac
 	
-	mtc1      $t7, $f1		        #converte int para float
+	mtc1      $t7, $f1		       #converte int para float
 	cvt.s.w   $f1, $f1
 	
-	sqrt.s    $f2,$f1		          #raiz quadrada 
+	sqrt.s    $f2,$f1		      #raiz quadrada 
 	
 	cvt.w.s   $f3, $f2            #converte o float em $f0 para inteiro no formato float em $f2
 	mfc1      $t8, $f3            #move o valor inteiro de $f2 para $t0
 	
-  neg       $t9,  $t0	          #converte b para -b
-  #x1
-  add       $t3, $t9, $t8
-  sll       $t4, $t1,  1       #2a
-  div       $t5, $t3,  $t4
-        
-  #espaco
-  li        $v0, 4             #código da syscall para imprimir string
+	neg       $t9,  $t0	          #converte b para -b
+	
+	#x1
+	add       $t3, $t9, $t8
+	sll       $t4, $t1,  1       #2a
+	div       $t5, $t3,  $t4
+	
+	#espaco
+	li        $v0, 4             #código da syscall para imprimir string
 	la        $a0, newline       #carrega o endereço da string "\n"
 	syscall   
-        
-  #x2 
-  sub       $t3, $t9, $t8
-  sll       $t4, $t1, 1        #2a
-  div       $t6, $t3, $t4
-        
-        
-  li 	      $v0, 4             #Comando.
+	
+	#x2 
+	sub       $t3, $t9, $t8
+	sll       $t4, $t1, 1        #2a
+	div       $t6, $t3, $t4
+	
+	
+	li 	      $v0, 4             #Comando.
 	la 	      $a0, memresult1    #Carrega string (endereço).
 	syscall
 	
-	move 	    $a0, $t5
+	move 	  $a0, $t5
 	li 	      $v0, 1
 	syscall
-
+	
 	li 	      $v0, 4             #Comando.
 	la 	      $a0, memresult2    #Carrega string (endereço).
 	syscall
 	
-	move 	$a0, $t6
-	li 	$v0, 1
+	move	  $a0, $t6
+	li		  $v0, 1
 	syscall
