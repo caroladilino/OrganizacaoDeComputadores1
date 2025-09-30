d7c5dc14e8e6ebbf09fb6bd53b486894abf7c0fb
Carolina Adilino <cmqadilino@gmail.com>
2025-09-30 16:45:52 -0300

    bug: erros sintaxe  **ainda não funciona

--- Lab3/Exc1.asm
+++ Lab3/Exc1.asm
@@ -1,9 +1,12 @@
 .data
-	msg1: .asciiz "Decida o número a ser calculado"
-	msg2: .asciiz "Decida o número de iterações"
+	msg1: .asciiz "Decida o número a ser calculado: "
+	msg2: .asciiz "Decida o número de iterações: "
+	msgdebug: .asciiz " loop rodou "
+	espaco: .asciiz "  "
+	divide: .asciiz "/"
 	
-	numeroUm: .float 1.00
-	numeroDois: .float 2.00
+	numeroUm: .double 1.00
+	numeroDois: .double 2.00
 
 .text
 main:
@@ -16,9 +19,9 @@ main:
 	la		$a0, msg1		# carregando um ponteiro para o endereço doq queremos escrever
 	syscall
 	
-	li		$v0, 6			# comando 6: read float
+	li		$v0, 6			# comando 7: read double
     syscall
-    mov.d  	$f2, $f0		# movendo para o reg f2 o valor inserido pelo usuário	
+    mov.d  	$f4, $f0		# movendo para o reg f4 o valor inserido pelo usuário
 	
 	
 	#---------------------------------------------#
@@ -31,38 +34,35 @@ main:
 	
 	li		$v0, 5			# comando 5: read int
     syscall
-    mov.d  	$f4, $f0		#movendo para o reg f4 o valor inserido pelo usuário		
+    move 	$t0, $v0		#movendo para o reg t0 o valor inserido pelo usuário	
+    	
     
     
-    l.d  	$f6, numeroUm	#salvando o número 1 no reg f5 (valor inicial da estimativa)
+    l.d  	$f6, numeroUm	#salvando o número 1 no reg f6 (valor inicial da estimativa)
     l.s 	$f14, numeroDois
     li		$t1, 0			#inicializando o contador de iterações
+    
 	jal		raiz_quadrada	#chama a função raiz quadrada
-	 
-	 
-	 #--------------------------------------------------------#
-	 # Função calcula raiz quadrada através do método de newton
-	 #--------------------------------------------------------#
-	 
-	 raiz_quadrada:
-	 	div.d 	$f8, $f2, $f4		# x/estimativa
-	 	add.d 	$f10, $f8, $f6		# somando o resultado da linha anterior com a estimativa
-	 	div.d  	$f12, $f10, $f14	# divindo por dois
-	 	mov.d 	$f6, $f12			# movendo p reg da estimativa o novo valor da estimativa
-	 	
-	 	addi	$t1, $t1, 1				#adicionando um ao contador de iterações
-	 	
-	 	bne		$t1, $t0, raiz_quadrada	#if (numero de iterações != n inserido pelo usuario) then repete procedimento
-	 	
 	
 	mov.d 	$f12, $f6
 	li		$v0, 2			# comando 2:escrever float
 	syscall
+	 
+	li		$v0, 10
+	syscall
+	 
+	 #------------------------------------------------------------#
+	 # Função calcula raiz quadrada através do método de newton
+	 #------------------------------------------------------------#
+	 
+	 raiz_quadrada:
+	 	div.d  	$f8, $f4, $f6		# x/estimativa
+	 	add.d 	$f10, $f8, $f6		# somando o resultado da linha anterior com a estimativa
+	 	div.d  	$f12, $f10, $f14	# divindo por dois		
 	 	
+	 	mov.d 	$f6, $f12			# movendo p reg da estimativa o novo valor da estimativa
 	 	
-	
-	
-	
-	
-	
-	
+	 	addi	$t1, $t1, 1				#adicionando um ao contador de iterações
+	 	bne		$t1, $t0, raiz_quadrada	#if (numero de iterações != n inserido pelo usuario) then repete procedimento
+	 	
+	 	jr	$ra
