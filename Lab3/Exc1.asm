d0db2edecbe9cfb6a08aaad9fa00ef41a81cddf7
Carolina Adilino <cmqadilino@gmail.com>
2025-10-01 00:02:40 -0300

    ainda não funciona

--- Lab3/Exc1.asm
+++ Lab3/Exc1.asm
@@ -3,10 +3,10 @@
 	msg2: .asciiz "Decida o número de iterações: "
 	msgdebug: .asciiz " loop rodou "
 	espaco: .asciiz "  "
-	divide: .asciiz "/"
 	
 	numeroUm: .double 1.00
 	numeroDois: .double 2.00
+
 
 .text
 main:
@@ -22,11 +22,9 @@ main:
 	li		$v0, 6			# comando 6: read double
     syscall
     
-	li		$v0, 3			# comando 2:escrever double
-	syscall
+    mov.d	$f12, $f0
+   
 
-	
-	
 	#---------------------------------------------#
 	# Recebendo do usuário o valor de iterações (n) 
 	#---------------------------------------------#
@@ -45,7 +43,7 @@ main:
     l.d 	$f14, numeroDois
     li		$a1, 0			#inicializando o contador de iterações como parâmetro
     
-	sqrt.d $f16, $f0
+	sqrt.d $f20, $f12
     
     
 	jal		raiz_quadrada	#chama a função raiz quadrada
@@ -60,7 +58,7 @@ main:
 	la		$a0, espaco		# carregando um ponteiro para o endereço doq queremos escrever
 	syscall
 	
-	mov.d 	$f12, $f16
+	mov.d 	$f12, $f20
 	li		$v0, 3			# comando 2:escrever double
 	syscall
 	 
@@ -72,11 +70,11 @@ main:
 	 #------------------------------------------------------------#
 	 
 	 raiz_quadrada:
-	 	div.d  	$f8, $f0, $f6		# x/estimativa
+	 	div.d  	$f8, $f12, $f6		# x/estimativa
 	 	add.d 	$f10, $f8, $f6		# somando o resultado da linha anterior com a estimativa
-	 	div.d  	$f12, $f10, $f14	# divindo por dois		
+	 	div.d  	$f16, $f10, $f14	# divindo por dois		
 	 	
-	 	mov.d 	$f6, $f12			# movendo p reg da estimativa o novo valor da estimativa
+	 	mov.d 	$f6, $f16			# movendo p reg da estimativa o novo valor da estimativa
 	 	
 	 	addi	$a1, $a1, 1				#adicionando um ao contador de iterações
 	 	bne		$a1, $a0, raiz_quadrada	#if (numero de iterações != n inserido pelo usuario) then repete procedimento
