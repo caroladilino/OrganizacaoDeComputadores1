3d5eaa2b9e8a13026781f3f7f8cfc704741a02f8
Carolina Adilino <cmqadilino@gmail.com>
2025-10-01 21:23:18 -0300

    bug: leitura do valor precisão dupla pelo teclado !!código funcionando

--- Lab3/Exc1.asm
+++ Lab3/Exc1.asm
@@ -19,38 +19,39 @@ main:
 	la		$a0, msg1		# carregando um ponteiro para o endereço doq queremos escrever
 	syscall
 	
-	li		$v0, 6			# comando 6: read double
+	li		$v0, 7			# comando 6: read double
     syscall
     
     mov.d	$f12, $f0
+    
    
 
 	#---------------------------------------------#
 	# Recebendo do usuário o valor de iterações (n) 
 	#---------------------------------------------#
 	
-	li		$v0, 4			#comando 4:escrever sting
-	la		$a0, msg2		#carregando um ponteiro para o endereço doq queremos escrever
+	li		$v0, 4			# comando 4:escrever sting
+	la		$a0, msg2		# carregando um ponteiro para o endereço doq queremos escrever
 	syscall
 	
 	li		$v0, 5			# comando 5: read int
     syscall
-    move 	$a0, $v0		#movendo para o reg a0 o valor inserido pelo usuário (p/ passar como parâmetro)	
+    move 	$a0, $v0		# movendo para o reg a0 o valor inserido pelo usuário (p/ passar como parâmetro)	
     	
     
     
-    l.d  	$f6, numeroUm	#salvando o número 1 no reg f6 (valor inicial da estimativa)
+    l.d  	$f6, numeroUm	# salvando o número 1 no reg f6 (valor inicial da estimativa)
     l.d 	$f14, numeroDois
-    li		$a1, 0			#inicializando o contador de iterações como parâmetro
+    li		$a1, 0			# inicializando o contador de iterações como parâmetro
     
 	sqrt.d $f20, $f12
     
     
-	jal		raiz_quadrada	#chama a função raiz quadrada
+	jal		raiz_quadrada	# chama a função raiz quadrada
 	
 
 	
-	mov.d 	$f12, $f0		#movedo para f12 o valor de retorno da função
+	mov.d 	$f12, $f0		# movedo para f12 o valor de retorno da função
 	li		$v0, 3			# comando 2:escrever double
 	syscall
 	
@@ -82,12 +83,3 @@ main:
 	 	mov.d	$f0, $f6				#movendo pro reg de retorno de double o valor da estimativa
 	 	
 	 	jr	$ra
-
-	 	
-	 	
-	
-	
-	
-	
-	
-	
